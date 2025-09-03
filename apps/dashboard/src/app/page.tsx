45d0b96c3467ea1cae864564b5be16edb4f3ac59
ami2730 <amanueld2730@gmail.com>
2025-09-03 13:16:35 +0300

    finish setup for project

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@repo/ui/components/button"
+import { Loader2Icon } from "lucide-react"
+
+
 export default function Home() {
   return (
-    <main className="flex h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">Ready for My Project</h1>
+    <main className="flex h-screen items-center justify-center ">
+     <div className="text-center items-center">
+       <h1 className="text-4xl font-bold text-blue-600">Ready for Doing</h1>
+         <Button size="sm" disabled className="flex">
+          <Loader2Icon className="animate-spin" />Please wait
+          </Button>
+      
+     </div>
     </main>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./apps/**/*.{ts,tsx}",
+    "./packages/**/*.{ts,tsx}",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
