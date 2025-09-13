6512fbac381b5b87860ac04523300d320d64cc8a
sifen <amanueld2730@gmail.com>
2025-09-13 10:29:54 +0300

    upgrade

--- packages/ui/src/components/StatsCard.tsx
+++ packages/ui/src/components/StatsCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardHeader, CardTitle, CardContent, CardDescription} from "./card";
-import {ArrowUpSquareIcon ,ArrowDownSquareIcon} from "lucide-react"
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
 interface StatsCardProps {
   title: string;
   value: string | number;
@@ -9,39 +10,10 @@ interface StatsCardProps {
 
 export function StatsCard({ title, value, change, positive }: StatsCardProps) {
   return (
-    <Card className="w-full shadow-sm rounded-2xl">
-      <CardHeader className="m-0 ">
-        <div className="flex">
-            {
-              positive ? <ArrowUpSquareIcon /> : <ArrowDownSquareIcon />
-            }
+    <Card className="flex">
+        <CardHeader className="m-0 p-0 ">
             
-            <CardTitle className=" font-medium text-muted-foreground">
-          {title}
-        </CardTitle>
-        </div>
-        
-      </CardHeader>
-      <div className="flex flex-col md:flex-row m-0 p-0">
-        <CardContent className="">
-        <p className="text-3xl font-bold">{value}</p>
-        
-      </CardContent>
-      <CardDescription className={`m-0 p-0 rounded-sm  ${
-              positive ? "bg-green-600/20" : "bg-red-600/20"
-            }`}>
-        {change && (
-          <p
-            className={`text-sm font-medium mt-1 ${
-              positive ? "text-green-600" : "text-red-600"
-            }`}
-          >
-            {change}
-          </p>
-        )}
-      </CardDescription>
-      </div>
-
+        </CardHeader>
     </Card>
   );
 }
